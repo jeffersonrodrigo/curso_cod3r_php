13d630dd48f5d785cee0a695f33630ae4bc6ef53
Jefferson_Rodrigo <jeffersonrodrigoaguiar@gmail.com>
2022-08-21 22:12:55 -0300

    aula 347 e 348

---
+++ usandoVar1.js
@@ -0,0 +1,20 @@
+ { 
+    { 
+        { 
+            { 
+                var sera = 'Será???'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local) // tentar descobrir como acessar a var dentro de uma função
