95bc118f96a5bf9338186308e93d68c6640488db
Jefferson_Rodrigo <jeffersonrodrigoaguiar@gmail.com>
2022-10-04 22:35:36 -0300

    subindo mais um pouco projeto final

--- projeto/innout-inicio/innout/public/index.php
+++ projeto/innout-inicio/innout/public/index.php
@@ -2,11 +2,4 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/user.php');
-
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-// echo $user->getSelect();
-
-echo User::getSelect(['id' => 1], 'name, email');
-echo '<br>';
-echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
